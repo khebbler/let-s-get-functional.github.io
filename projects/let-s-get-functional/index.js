1168bd96ec2052f16d425fc82d2ee61ebb84ee0d
khebbler <khebbler@gmail.com>
2024-08-01 15:41:01 -0500

    solved youngest and oldest

--- projects/let-s-get-functional/index.js
+++ projects/let-s-get-functional/index.js
@@ -3,7 +3,7 @@
 'use strict';
 
 var customers = require('./data/customers.json');
-var _ = require('underbar');
+var _ = require('underbar'); // _ = {filter: ?, map:}
 
 /**
  * 1. Import your lodown module using the require() method,
@@ -21,15 +21,94 @@ var _ = require('underbar');
  *    IMPORTANT: Make sure you replace <YOUR_GITHUB_FOLDER with your actual github folder name that is in your workspace.
  */
 
-var maleCount = function(array) {
+/*var maleCount = function(array) {
+    // Initializing count variable
+    let count = 0;
+    // Iterating through array
+    for (let i = 0; i < array.length; i++) {
+        // Checking if customer is male
+        if (array[i].gender === 'male') {
+            // Incrementing count variable
+            count++;
+        }
+    }
+    // Returning count variable
+    return count;
+};
+*/
 
+var maleCount = function(array) {
+    let males = _.filter(array, function(customer) {
+        return customer.gender === 'male';
+    })
+    return males.length;
+}
+
+
+var femaleCount = function(array){
+    let females = _.filter(array, function(customer) {
+        return customer.gender === 'female';
+    });
+    return females.length;
+}
+/*
+// using reduce
+var femaleCount = function(array){
+    let females = _.reduce(array, function(accumulator, current){
+        // accumulator = 1
+        if (curernt.gender === 'female') {
+            accumulator += 1; // accumulator = 2
+        }
+        return accumulator; // return 2
+    }, 0);
+    return females;
+};
+*/
+
+
+/*
+// callback
+function(accumulator, current){
+
+}, 0);
+
+// seed => procides your return an initial datatype
+    // need to return a number => make a seed of 0
+    // need to return an array => mae a seed of []
+
+*/
+
+var oldestCustomer = function(array) {
+    // Finding customer with highest age using reduce function
+    let oldest = _.reduce(array, function(acc, customer) {
+        // Checking if customer's age is higher than accumulator's age
+        if (customer.age > acc.age) {
+            // Returning current customer if true
+            return customer;
+        } else {
+            // If not, returning accumulated customer
+            return acc;
+        }
+    })
+    // Returning name of oldest customer
+    return oldest.name;
 };
 
-var femaleCount;
-
-var oldestCustomer;
-
-var youngestCustomer;
+var youngestCustomer = function(array) {
+    // Finding customer with the lowest age using reduce function
+    let youngest = _.reduce(array, function(acc, customer) {
+        // Checking if customer's age is lower than accumulator's age
+        if (customer.age < acc.age) {
+            // Returning current customer if true
+            return customer;
+        } else {
+            // If not, returning accumulated customer
+            return acc;
+        }
+    })
+    // Returning name of youngest customer
+    return youngest.name;
+};
 
 var averageBalance;
 
